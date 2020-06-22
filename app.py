79b68e2e85a8e96c9ff7edca13f39743469479e7
Nicholas Sharp <naas8905@gmail.com>
2020-06-22 10:04:04 -0700

    Adding the table

--- app.py
+++ app.py
@@ -6,13 +6,30 @@ from flask_heroku import Heroku
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = ""
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://sfpidbnchhmftd:a66a9d829a0e814a66837d4f9285de8f86a3262efb6e27f33425ffed05035fff@ec2-34-230-231-71.compute-1.amazonaws.com:5432/ddng22t6uds9fs"
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 heroku = Heroku(app)
 CORS(app)
 
+
+class Month(bd.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    position = db.Column(db.Integer, unique=True, nullable=False)
+    month = db.Column(db.String, nullable=False)
+    daysInMonth = db.Column(db.Integer, nullable=False)
+    daysInPreviousMonth = db.Column(db.Integer, nullable=False)
+    startDay = db.Column(db.Integer, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, position, month, daysInMonth, daysInPreviousMonth, startDay, year):
+        self.position = position
+        self.month = month
+        self.daysInMonth= daysInMonth
+        self.daysInPreviousMonth = daysInPreviousMonth
+        self.startDay = startDay
+        self.year = year
 
 if __name__ == "__main__":
     app.debug = True
