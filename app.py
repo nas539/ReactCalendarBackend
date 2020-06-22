d85b5e88c29d993efba9004792526cb8f4eea033
Nicholas Sharp <naas8905@gmail.com>
2020-06-22 09:06:33 -0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from flask_cors import CORS
+from flask_heroku import Heroku
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = ""
+
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+heroku = Heroku(app)
+CORS(app)
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
